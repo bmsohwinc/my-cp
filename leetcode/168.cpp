441ac48f125d1480b375b22a201d1f8ac8ec7d06
Sourabha Bharadwaj B M <36269370+bmsohwinc@users.noreply.github.com>
2023-07-09 20:11:44 +0530

    168. Excel Sheet Column Title

--- leetcode/168.cpp
+++ leetcode/168.cpp
@@ -4,7 +4,7 @@ public:
         string s = "";
         int n = columnNumber;
         while (n > 0) {
-            n--; // Because, in Excel column header, 0 is missing.
+            n--; // Because, in the Excel column header, 0 is missing.
             int rem = n % 26; // Similar to what we do during decimal to binary
             n /= 26; // Similar to what we do during decimal to binary
             s += ('A' + rem);
