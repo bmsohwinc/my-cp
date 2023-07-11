a12cccf27326b0caf25f16e6492d7afb3e482c30
Sourabha Bharadwaj B M <36269370+bmsohwinc@users.noreply.github.com>
2023-07-11 17:00:01 +0530

    Convert array into Zig-Zag fashion

--- gfg/convert-array-into-zig-zag-fashion1638.cpp
+++ gfg/convert-array-into-zig-zag-fashion1638.cpp
@@ -1,5 +1,19 @@
 class Solution {
   public:
+
+    // Time: O (N)
+    void zigZag(int arr[], int n) {
+        for (int i = 0; i + 1 < n; i++) {
+            if (i % 2 == 0 and arr[i + 1] < arr[i]) {
+                swap(arr[i], arr[i + 1]);
+            }
+            
+            if (i % 2 == 1 and arr[i + 1] > arr[i]) {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+    }
+
     // Program for zig-zag conversion of array
     // Time: O (N log N)
     void zigZag(int arr[], int n) {
